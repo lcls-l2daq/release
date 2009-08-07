faac4eba53c97c04d7b6ff7167adf3325b802813
jackp <none@example.com>
2009-08-07 01:23:20 +0000

    Thu Aug  6 18:22:39 PDT 2009 jackp moved some things around and pointed RTEMS at the pcds package directory.

--- projects.mk
+++ projects.mk
@@ -1,20 +1,28 @@
-# NB this is an example projects.mk.  You should edit to build what you want !!!!
 # List of projects (low level first)
-#projects := rtems \
-#            rce \
-#            rceusr \
-#            rceapp
+projects := rtems \
+            rce \
+            rceusr \
+            rceapp
 #            rcehw
 
+rtems_use := /reg/g/pcds/package/rtems/4.9.2
+#rtems_use := ~/rtems/4.9.2
+
+rce_use    := release
+rceusr_use := release
+rceapp_use := release
+rcehw_use  := release
+
 projects := pdsdata \
-	    qt \
-	    qwt \
-	    epics \
 	    acqiris \
 	    evgr \
 	    leutron \
 	    pds \
+	    qt \
+	    qwt \
+	    epics \
 	    pdsapp
+
 
 epics_use   := /reg/g/pcds/package/external/epicsca-pcds-R1.0-r410
 acqiris_use := /reg/g/pcds/package/external/acqiris_V00-00-00
@@ -22,14 +30,7 @@ evgr_use := /reg/g/pcds/package/external/evgr_V00-00-02
 leutron_use := /reg/g/pcds/package/external/leutron_V00-00-00
 qt_use := /reg/g/pcds/package/qt-4.3.4
 qwt_use := /reg/g/pcds/package/external/qwt-5.1.1
+
 pds_use := release
 pdsdata_use := release
 pdsapp_use := release
-
-
-
-rtems_use := /afs/slac.stanford.edu/g/npa/package/rtems/4.9.2
-rce_use    := release
-rceusr_use := release
-rceapp_use := release
-rcehw_use  := release
