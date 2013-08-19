1a05f76cd0c5d6f63ed72b9dd076bb3e15be256c
weaver <none@example.com>
2013-08-19 21:01:01 +0000

    Further EPIX monitoring specializations

--- projects.mk
+++ projects.mk
@@ -1,5 +1,13 @@
 # List of projects (low level first)
 
+boost_use      := /reg/common/package/boost/1.49.0-python2.7/x86_64-rhel5-gcc41-opt/
+ndarray_use    := /reg/common/package/ndarray/1.1.3/x86_64-rhel5-gcc41-opt
+
+boost_use_include    := $(boost_use)/include
+boost_use_lib_x86_64 := $(boose_use)/lib
+
+ndarray_use_include := $(ndarray_use)
+
 #
 # RTEMS
 #
@@ -27,6 +35,8 @@ endif
 #
 ifneq ($(findstring i386-linux,$(tgt_arch)),)
 projects := pdsdata \
+      boost \
+      ndarray \
       acqiris \
       evgr \
       leutron \
@@ -78,6 +88,8 @@ endif
 #
 ifneq ($(findstring x86_64,$(tgt_arch)),)
 projects := pdsdata \
+      boost \
+      ndarray \
       qwt \
       epics \
       evgr \
