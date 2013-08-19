54422bdd2b22874ab54d2cc153eee64e5a906384
weaver <none@example.com>
2013-08-19 19:28:33 +0000

    Change to make system to create build/<external> directories

--- make/share/release.mk
+++ make/share/release.mk
@@ -23,17 +23,34 @@ cleanall:
 	$(quiet)$(RM) -r build
 
 define soft-link
-  if [ -e $(1) ]; then \
-    rm $(1); \
-  else \
-    echo '[SL] Will make soft link $(1)'; \
-  fi
-  if [ -e $(2) ]; then \
-    ln -s $(2) $(1); \
-  else \
-    echo '[SL] *** project $(2) not found'; \
+  if [ ! -e $(1) ]; then \
+    if [ -e $(2) ]; then \
+      if [ ! -z $(2) ]; then \
+        echo '[SL] Make soft link $(1) $(2)'; \
+        ln -sf $(2) $(1);     \
+      fi \
+    else \
+      echo '[SL] *** project $(2) not found'; \
+    fi \
   fi
 endef
+
+define build-ext-dir
+  if [ ! -e $(1) ]; then \
+    echo '[XD] Make external link directory $(1)'; \
+    mkdir -p $(1); \
+    mkdir -p $(1)/lib; \
+  fi
+  $(call soft-link,$(1)/include,$($(2)_use_include))
+  $(call soft-link,$(1)/lib/i386-linux    ,$($(2)_use_lib_i386))
+  $(call soft-link,$(1)/lib/i386-linux-opt,$($(2)_use_lib_i386))
+  $(call soft-link,$(1)/lib/i386-linux-dbg,$($(2)_use_lib_i386))
+  $(call soft-link,$(1)/lib/x86_64-linux    ,$($(2)_use_lib_x86_64))
+  $(call soft-link,$(1)/lib/x86_64-linux-opt,$($(2)_use_lib_x86_64))
+  $(call soft-link,$(1)/lib/x86_64-linux-dbg,$($(2)_use_lib_x86_64))
+endef
+
+#    $(call soft-link,$(1)/lib/$$arch,$($(2)_use_lib_$$arch)); 
 
 define delete-soft-links
   for prj in $(projects); do \
@@ -49,11 +66,16 @@ $(1).%: dir
 	$(quiet)echo "[PR] Target <$$*> project <$(1)>"
 	$$(MAKE) -C $(1) $$*
 else
+ifneq ($$(findstring /,$$($(1)_use_include),),)
+$(1).%: dir
+	@$$(call build-ext-dir,build/$(1),$(1))
+else
 ifneq ($$(findstring /,$$($(1)_use),),)
 $(1).%: dir
 	@$$(call soft-link,build/$(1),$$($(1)_use))
 else
 $$(error 'Project $(1) lacks a use statement')
+endif
 endif
 endif
 endef
