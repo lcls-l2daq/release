f555a1258fd7ac63cfd86c4727ccf65b5ea4b73d
weaver <weaver@SLAC.STANFORD.EDU>
2014-09-15 16:44:26 +0000

    Updated psalg, pdsdata references

--- projects.mk
+++ projects.mk
@@ -31,8 +31,8 @@ picam_use      := /reg/g/pcds/package/external/picam-2.6.1
 gsl_use        := /reg/g/pcds/package/external/gsl-1.13
 boost_use      := /reg/common/package/boost/1.49.0-python2.7/x86_64-rhel5-gcc41-opt/
 ndarray_use    := /reg/common/package/ndarray/1.1.3/x86_64-rhel5-gcc41-opt
-psalg_use      := /reg/common/package/psalg/1.0.7
-pdsdata_use    := /reg/common/package/pdsdata/8.1.0
+psalg_use      := /reg/common/package/psalg/1.0.8
+pdsdata_use    := /reg/common/package/pdsdata/8.1.2
 
 #
 #  *_use_include definitions will create a directory structure under build for
