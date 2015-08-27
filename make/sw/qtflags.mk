65c7ddc261292facbbba087ed74796dc9d41643e
weaver <weaver@SLAC.STANFORD.EDU>
2015-08-27 17:34:47 +0000

    Fix to qt since rhel7 addition

--- make/sw/qtflags.mk
+++ make/sw/qtflags.mk
@@ -1,12 +1,16 @@
-ifneq ($(findstring linux,$(tgt_arch)),$(findstring rhel7,$(tgt_arch)),)
+ifneq ($(findstring linux,$(tgt_arch)),)
+qtincdir  := qt/include
 qtlibdir  := qt/QtGui qt/QtCore
 qtslibdir :=
-#ifneq ($(findstring x86_64,$(tgt_arch)),)
-#qtincdir  := qt/include_64
-#else
-qtincdir  := qt/include
-#endif
+else
+ifneq ($(findstring rhel6,$(tgt_arch)),)
+qtincdir  := 
+qtlibdir  := 
+qtslibdir := $(USRLIBDIR)/QtGui $(USRLIBDIR)/QtCore
+endif
+endif
 
+ifneq ($(qtincdir),)
 # qwt includes qt headers without package prefix!
 qwtincs := $(qtincdir)/Qt
 qwtincs += $(qtincdir)/Qt3Support
@@ -23,15 +27,7 @@ qwtincs += $(qtincdir)/QtTest
 qwtincs += $(qtincdir)/QtUiTools
 qwtincs += $(qtincdir)/QtXml
 qwtsinc :=
-
 else
-
-ifneq ($(findstring rhel6,$(tgt_arch)),)
-qtincdir  := 
-qtlibdir  := 
-qtslibdir := $(USRLIBDIR)/QtGui $(USRLIBDIR)/QtCore
-endif
-
 qwtincs :=
 qwtsinc := /usr/include/Qt
 qwtsinc += /usr/include/Qt3Support
@@ -47,5 +43,4 @@ qwtsinc += /usr/include/QtSvg
 qwtsinc += /usr/include/QtTest
 qwtsinc += /usr/include/QtUiTools
 qwtsinc += /usr/include/QtXml
-
 endif
